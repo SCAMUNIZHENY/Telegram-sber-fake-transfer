49aa7b1e8bc1189ec3de0dd385dc0521ab9492b9
Kotorkovsciy <kotorkovsciy@gmail.com>
2022-08-14 04:45:48 +0300

    Added name input

--- handlers/client.py
+++ handlers/client.py
@@ -9,6 +9,7 @@ from os import remove
 
 class Price(StatesGroup):
     user_id = State()
+    name = State()
     price = State()
 
 
@@ -24,6 +25,11 @@ async def cmd_cancel(message: types.Message, state: FSMContext):
 async def cmd_trans(message: types.Message, state: FSMContext):
     await Price.user_id.set()
     await state.update_data(user_id=message.from_user.id)
+    await Price.name.set()
+    await message.answer('Введите ФИО', reply_markup=kb_cancel)
+
+async def name_step(message: types.Message, state: FSMContext):
+    await state.update_data(name=message.text)
     await Price.price.set()
     await message.answer('Введите сумму', reply_markup=kb_cancel)
 
@@ -31,7 +37,7 @@ async def cmd_trans(message: types.Message, state: FSMContext):
 async def send(message: types.Message, state: FSMContext):
     await state.update_data(price=message.text)
     user_data = await state.get_data()
-    await message.answer_photo(await translate(user_data["user_id"], user_data["price"]), reply_markup=kb_client)
+    await message.answer_photo(await translate(user_data["user_id"], user_data["name"], user_data["price"]), reply_markup=kb_client)
     remove(f"images/{user_data['user_id']}.jpg")
     await state.finish()
 
@@ -42,5 +48,7 @@ def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(cmd_cancel, Text(
         equals="отмена", ignore_case=True), state="*")
     dp.register_message_handler(cmd_trans, Text(equals='Перевод'), state="*")
+    dp.register_message_handler(
+        name_step, state=Price.name, content_types=types.ContentTypes.TEXT)
     dp.register_message_handler(
         send, state=Price.price, content_types=types.ContentTypes.TEXT)
--- scripts/trans.py
+++ scripts/trans.py
@@ -2,7 +2,7 @@ from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
 
 
-async def translate(id_user, price):
+async def translate(id_user, name, price):
     sber = Image.open("images/sber.jpg")
 
     idraw = ImageDraw.Draw(sber)
@@ -13,12 +13,17 @@ async def translate(id_user, price):
     for i in range(len(price)):
         sx -= 10
     price = f"{price} ₽"
+    sy = 208
+    for i in range(len(name)):
+        sy -= 4
 
     font = ImageFont.truetype("arial.ttf", size=20)
     font2 = ImageFont.truetype("arialbd.ttf", size=50)
+    font3 = ImageFont.truetype("arial.ttf", size=20)
 
     idraw.text((10, 7), time, font=font)
     idraw.text((sx, 310), price, font=font2)
+    idraw.text((sy, 390), name, font=font3)
     sber.save(f'images/{id_user}.jpg')
     img = open(f'images/{id_user}.jpg', "rb")
     return img
